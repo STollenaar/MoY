8bb28193999463af20edf6c2924cd4092afc5457
Stephen Tollenaar <stephen@tollenaar.com>
2015-06-27 17:33:37 -0230

    added

---
+++ src/MoY/tollenaar/stephen/InventoryUtils/InventoryType.java
@@ -0,0 +1,35 @@
+package MoY.tollenaar.stephen.InventoryUtils;
+
+public enum InventoryType {
+		ALLKILL("AllKill"),
+		ALLHARVEST("AllHarvest"),
+		ALLTALKTO("AllTalkTo"),
+		WARPLIST("WarpList"),
+		ALLWARPS("AllWarps"),
+		TALKTOQUEST("TalkToQuest"),
+		KILLQUEST("KillQuest"),
+		HARVESTQUEST("HarvestQuest"),
+		MAINSETTING("Main settings"),
+		EVENTQUEST("EventQuest"),
+		ALLEVENTS("AllEvents"),
+	;
+
+	private final String name;
+
+	InventoryType(String name) {
+		this.name = name;
+	}
+
+	public static boolean contains(String type){
+		for(InventoryType in : values()){
+			if(in.getname().equals(type)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private String getname() {
+		return name;
+	}
+}
